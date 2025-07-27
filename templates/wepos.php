39efa72f42bda33ed845aeb3385f225331087bfe
omairhussainpk <92503007+omairhussainpk@users.noreply.github.com>
2025-07-27 15:21:03 -0700

    Add files via upload

---
+++ templates/wepos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    
+    <?php wp_head(); ?>
+    
+
+</head>
+<body>
+    <section>
+	 
+	 
+ <div class="theshoolpos" id="vue-frontend-app"></div>
+<div class="FooterAPP">
+<a class="buttondetail" href="https://parveen.theschool.pk/student" target="_blank">Student/Staff Details</a>
+<a class="buttondetail" href="https://parveen.theschool.pk/visiter-details" target="_blank">Visitor Details</a>
+<a class="buttondetail" href="https://parveen.theschool.pk/visitor-form" target="_blank">Online Visitor Form</a>
+<a class="buttondetail" href="https://parveen.theschool.pk/online-admission-form" target="_blank">Online Admission Form</a></div>
+
+
+</section>
+
+
+    <?php wepos_footer(); ?>
+</body>
+</html>
